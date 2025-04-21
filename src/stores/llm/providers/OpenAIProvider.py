310bd60300ab1d9620d1107a0a904ded25bacfb8
Abdllah Elhoseny <3bdallah.a7meed@gmail.com>
2025-04-22 00:51:25 +0200

    .

--- src/stores/llm/providers/OpenAIProvider.py
+++ src/stores/llm/providers/OpenAIProvider.py
@@ -1,2 +1,10 @@
-from ..LLMinterface import LLLLMinterface
+from ..LLMinterface import LLMinterface
 from openai import OpenAI
+
+class OpenAIProvider(LLMinterface):
+
+    def __init__(self, api_key: str, api_url: str=None,
+                 default_input_max_characters: int=1000,
+                 default_generation_max_output_tokens: int=1000,
+                 default_generation_temperature: float=0.1):
+        pass
