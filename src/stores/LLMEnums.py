c944075d61a97779e113b11ed8005fca011fd4bd
Abdllah Elhoseny <3bdallah.a7meed@gmail.com>
2025-04-28 00:43:24 +0300

    .

--- src/stores/LLMEnums.py
+++ src/stores/LLMEnums.py
@@ -5,4 +5,8 @@ class LLMEnums(Enum):
     OPENAI = "OPENAI"
     COHERE = "COHERE"
 
+class OpenAIEnum(Enum):
+    SYSTEM = "system"
+    USER = "user"
+    ASSISTANT = "assistant"
     
--- src/stores/LLMinterface.py
+++ src/stores/LLMinterface.py
@@ -7,11 +7,11 @@ class LLMinterface(ABC):
         pass
 
     @abstractmethod
-    def set_embedding_model(self, model_id: str):
+    def set_embedding_model(self, model_id: str, embedding_size: int):
         pass
 
     @abstractmethod
-    def generate_text(self, prompt: str, max_output_tokens: int, 
+    def generate_text(self, prompt: str, chat_history: list=[], max_output_tokens: int=None, 
                       temperature: float = None):
         pass
 
