1341ec06d1c68a16ded523d730a7ff5e98884c42
Abdllah Elhoseny <3bdallah.a7meed@gmail.com>
2025-05-08 15:09:41 +0300

    .

--- src/stores/LLMinterface.py
+++ src/stores/LLMinterface.py
@@ -16,7 +16,7 @@ class LLMinterface(ABC):
         pass
 
     @abstractmethod
-    def embed_text(self, text: str, document_type: str):
+    def embed_text(self, text: str, document_type: str = None):
         pass
     
     @abstractmethod
