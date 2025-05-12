e67cb05ce0896ec28466563af499e74751cdca57
Abdllah Elhoseny <3bdallah.a7meed@gmail.com>
2025-05-12 17:50:27 +0300

    15

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -27,6 +27,10 @@ class Settings(BaseSettings):
     DEFAULT_GENERATION_MAX_OUTPUT: int = None
     DEFAULT_GENERATION_TEMPERATURE: float = None
 
+    VECTOR_DB_BACKEND: str
+    VECTOR_DB_PATH: str
+    VECTOR_DB_DISTANCE_METHOD: str = None
+
     class Config:
         env_file = ".env"
 
---
+++ src/stores/vectordb/providers/__init__.py
@@ -0,0 +1,1 @@
+from .QdranrDBProvider import QdrantDBProvider
